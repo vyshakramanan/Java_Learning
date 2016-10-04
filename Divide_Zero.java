b82cd108056b94e1af08b32dca23bf080e41ce36
vyshakramanan <vyshak.venkataramanan@gmail.com>
2016-10-03 22:17:28 -0500

    Divide_Zero

---
+++ Divide_Zero.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+	double x=50.0;
+        double y = 0.0;
+        double z = -50.0;
+        double k=x/y;
+        System.out.println(k);
+        k=z/y;
+        System.out.println(k);
+    }
+}
